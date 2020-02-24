728131a351ce1d05c393ceb2d9eef107fd5c2cb9
jorgdiaz1908 <48842627+jorgdiaz1908@users.noreply.github.com>
2020-02-24 13:05:11 -0500

    problema 7 y problema 8

---
+++ ejercicios/p7.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h> //rand() y srand()
+#include <time.h>
+
+
+int main (int argc, char const *argv[]) {
+    srand(time(NULL));
+    int number1, number2, result;
+    number1 = rand() % 10 + 1;
+    number2 = rand() % 10 + 1;
+    do{
+        printf("¿Cuánto es %d veces %d? Para terminar ingrese como  resultado '0'\n", number1, number2);
+        scanf("%d", &result);
+        if(result == number1*number2){
+            printf("Muy bien! \n");
+            number1 = rand() % 10 + 1;
+            number2 = rand() % 10 + 1;
+        }else{
+            if(result != 0){
+                printf("No. Por favor intenta nuevamente \n");
+            }
+        }
+    }while (result != 0);
+    return 0;
+}
